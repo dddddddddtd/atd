dd5eb24a931b6ffaf34bd4e9bced3e65466c84e6
koot <kozjach@gmail.com>
2022-12-07 20:06:23 +0100

    test

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def read_file(filename):
     return dmus, values
 
 
-dmus, inputs = read_file("inputs.csv")
-_, outputs = read_file("outputs.csv")
+dmus, inputs = read_file("crossinputs.csv")
+_, outputs = read_file("crossoutputs.csv")
 # dmus, inputs = read_file("inputstest.csv")
 # _, outputs = read_file("outputstest.csv")
 # dmus, inputs = read_file("hcuinput.csv")
@@ -46,7 +46,47 @@ def dmu_problem_effectivenes(dmu, inputs, outputs):
     for x in range(dmu_count):
         dmu_problem += make_affine(u, outputs[x, :]) <= make_affine(v, inputs[x, :])
 
+    dmu_problem.solve()
+    for v in dmu_problem.variables():
+        print(v.name, "=", v.varValue)
     return dmu_problem
+
+def super_effectivenes(dmu, inputs, outputs):
+    dmu_problem = pulp.LpProblem(f'DMU_{dmu}', pulp.LpMaximize)
+
+    v = pulp.LpVariable.dicts("v", list(range(1, input_count + 1)), 0)
+    u = pulp.LpVariable.dicts("u", list(range(1, output_count + 1)), 0)
+
+    dmu_problem += make_affine(u, outputs[dmu, :])
+    dmu_problem += make_affine(v, inputs[dmu, :]) == 1
+
+    for x in range(dmu_count):
+        if x != dmu:
+            dmu_problem += make_affine(u, outputs[x, :]) <= make_affine(v, inputs[x, :])
+
+    dmu_problem.solve()
+    return dmu_problem.objective.value()
+
+def optimal_vector(dmu, eff, inputs, outputs):
+    dmu_problem = pulp.LpProblem(f'DMU_{dmu}', pulp.LpMinimize)
+
+    v = pulp.LpVariable.dicts("v", list(range(1, input_count + 1)), 0)
+    u = pulp.LpVariable.dicts("u", list(range(1, output_count + 1)), 0)
+
+    dmu_problem += make_affine(u, np.vstack([outputs[:dmu], outputs[dmu+1:]]).sum(0))
+    dmu_problem += make_affine(v, np.vstack([inputs[:dmu], inputs[dmu+1:]]).sum(0)) == 1
+
+    for x in range(dmu_count):
+        if x != dmu:
+            dmu_problem += make_affine(u, outputs[x, :]) <= make_affine(v, inputs[x, :])
+
+    dmu_problem += make_affine(u, outputs[dmu, :]) == eff * make_affine(v, inputs[dmu, :])
+
+    print(dmu_problem)
+    dmu_problem.solve()
+    for v in dmu_problem.variables():
+        print(v.name, "=", v.varValue)
+    return dmu_problem.objective.value()
 
 
 def solve_hcu(dmu, inputs, outputs):
@@ -73,16 +113,27 @@ def solve_hcu(dmu, inputs, outputs):
 
 np.set_printoptions(precision=2)
 
-solution_hcu = {}
-for i, dmu in enumerate(dmus):
-    solution_hcu[dmu] = solve_hcu(i, inputs, outputs)
+# solution_hcu = {}
+# for i, dmu in enumerate(dmus):
+#     solution_hcu[dmu] = solve_hcu(i, inputs, outputs)
+#
+# for i, dmu in enumerate(dmus):
+#     print(f'{dmu} & {np.round(solution_hcu[dmu][0], 2)} \\\\')
+#
+# print()
+# for i, dmu in enumerate(dmus):
+#     if solution_hcu[dmu][0] < 1:
+#         hcu = solution_hcu[dmu][1]
+#         print(
+#             f'{dmu} & {" & ".join(list(np.round(hcu, 2).astype(str)))} & {" & ".join(list(np.round(inputs[i] - hcu, 2).astype(str)))} \\\\')
 
-for i, dmu in enumerate(dmus):
-    print(f'{dmu} & {np.round(solution_hcu[dmu][0], 2)} \\\\')
+# seff = [super_effectivenes(x, inputs, outputs) for x in range(len(dmus))]
+#
+# for i, dmu in enumerate(dmus):
+#     print(f'{dmu} & {np.round(seff[i], 2)} \\\\')
+##          f'{np.round(solution_hcu[dmu][0], 2)} \\\\')
 
-print()
-for i, dmu in enumerate(dmus):
-    if solution_hcu[dmu][0] < 1:
-        hcu = solution_hcu[dmu][1]
-        print(
-            f'{dmu} & {" & ".join(list(np.round(hcu, 2).astype(str)))} & {" & ".join(list(np.round(inputs[i] - hcu, 2).astype(str)))} \\\\')
+effs = [solve_hcu(dmu, inputs, outputs)[0] for dmu in range(dmu_count)]
+
+dmu_problem_effectivenes(0, inputs, outputs)
+optimal_vector(0, effs[0], inputs, outputs)
